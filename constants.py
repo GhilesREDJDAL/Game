e52dc872a049c21a29b4dd0830d43d4248f3cd68
REDJDAL-GhilesGitHub <redjdalghiles2@gmail.com>
2024-12-08 14:58:36 +0000

    Modification de la classe movement et fichiers associés

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # Définition des constantes
-GRID_SIZE = 8  # Taille de la grille
-CELL_SIZE = 60  # Taille des cases en pixels
+GRID_SIZE = 12  # Taille de la grille
+CELL_SIZE = 40  # Taille des cases en pixels
 WIDTH = GRID_SIZE * CELL_SIZE  # Largeur de la fenêtre
 HEIGHT = GRID_SIZE * CELL_SIZE  # Hauteur de la fenêtre
 FPS = 30  # Taux de rafraîchissement
